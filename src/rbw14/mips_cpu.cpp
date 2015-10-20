df0abce34731cf73289af46179e0cc4faf3ed8f2
Ravi Woods <ravi.woods@gmail.com>
2015-10-20 16:26:46 +0100

    Added DT's source

--- src/rbw14/mips_cpu.cpp
+++ src/rbw14/mips_cpu.cpp
@@ -1,53 +1,18 @@
 #include "mips.h"
-#include "mips_hdr.h"
 
-using namespace std;
-
-struct mips_cpu_impl{
-
-	uint32_t pc;
-	uint32_t pcN;
-	uint32_t regs[32];
-	FILE* debug_out;
-	mips_mem_h mem;
+struct mips_cpu_impl
+{
+    uint32_t pc;
+    uint32_t pcNext;
+    
+    uint32_t regs[32];
+    
+    mips_mem_h mem;
+    
+    unsigned logLevel;
+    FILE *logDst;
 };
 
-mips_cpu_h mips_cpu_create(mips_mem_h mem)
-{
-	unsigned i;
-	//mips_cpu_h res=(mips_cpu_h)malloc(sizeof(struct mips_cpu_impl));
-	mips_cpu_h res = new mips_cpu_impl;
-	res->mem=mem;
-//
-	res->pc=0;
-	res->pcN=4;	// NOTE: why does this make sense?
-	
-	//
-	if (DEBUG_LEVEL != 0) {
-		cout << "YES" << endl;
-		time_t rawtime;
-		struct tm * timeinfo;
-		char buffer [50];
-		time (&rawtime);
-		timeinfo = localtime (&rawtime);
-		strftime(buffer,50,"./src/rbw14/debug/debug_%d-%m-%y_%H-%M-%S.txt",timeinfo);
-		res->debug_out = fopen(buffer, "w+");
-	} else {
-		cout << "No" << endl;
-		res->debug_out = tmpfile();
-	}
-	
-	for( i=0;i<32;i++){
-		res->regs[i]=0;
-	}
-
-	return res;
-}
-
-void mips_cpu_free(mips_cpu_h state)
-{
-	free(state);
-}
 
 mips_error mips_cpu_get_register(
 	mips_cpu_h state,	//!< Valid (non-empty) handle to a CPU
@@ -55,15 +20,9 @@ mips_error mips_cpu_get_register(
 	uint32_t *value		//!< Where to write the value to
 )
 {
-	if(state==0)
-		return mips_ErrorInvalidHandle;
-	if(index>=32)
-		return mips_ErrorInvalidArgument;
-	if(value==0)
-		return mips_ErrorInvalidArgument;
-
-	*value = state->regs[index];
-	return mips_Success;
+    // TODO : error handling
+    *value = state->regs[index];
+    return mips_Success;
 }
 
 /*! Modifies one of the 32 general purpose MIPS registers. */
@@ -73,53 +32,97 @@ mips_error mips_cpu_set_register(
 	uint32_t value		//!< New value to write into register file
 )
 {
-	if(state==0)
-		return mips_ErrorInvalidHandle;
-	if(index>=32)
-		return mips_ErrorInvalidArgument;
-
-	// TODO : What about register zero?
-	state->regs[index]=value;
-
-	return mips_Success;
+    // TODO : error handling
+    state->regs[index] = value;
+    return mips_Success;
 }
 
-/*
-mips_error mips_cpu_set_debug_level(mips_cpu_h state, unsigned level, FILE *dest) {
-	try {
-		if (level != 0) {
-			cout << "YES" << endl;
-			dest = state->debug_out;
-		} else {
-			cout << "NO" << endl;
-		}
-		return mips_Success;
-	} catch (mips_error e){
-		return e;
-	}
-}
-*/
-
-mips_error mips_cpu_step(mips_cpu_h state)
+mips_error mips_cpu_set_debug_level(mips_cpu_h state, unsigned level, FILE *dest)
 {
-	uint32_t pc=state->pc;
-	uint32_t length = 4;
-	if(state==0)
-		return mips_ErrorInvalidHandle;
-	//TODO: Here is where the magic happens
-	// - Fetch the instruction from memory (mips_mem_read)
-	uint8_t num = 0xFF;
-	uint8_t *data_in = &num;
-	uint8_t *data_out = new uint8_t;
-	mips_error ravi;
-	ravi = mips_mem_write(state->mem, pc, length, data_in);
-	ravi = mips_mem_read(state->mem, pc, length, data_out);
-	cout << "data_in is " << (bitset<8>(*data_in)) << "." << endl;
-	cout << "data_out is " << (bitset<8>(*data_out)) << "." << endl;
-	fprintf(state->debug_out,"data out is %X\n", *data_out);
-	// - Decode the instruction (is it R, I, J)?
-	// - Execute the instruction (do maths, access memory, ...)
-	// - Writeback the results (update registers, advance pc)
+    state->logLevel = level;
+    state->logDst = dest;
+    return mips_Success;
+}
 
-	return mips_ErrorNotImplemented;
+mips_cpu_h mips_cpu_create(mips_mem_h mem)
+{
+    mips_cpu_impl *cpu=new mips_cpu_impl;
+    
+    cpu->pc=0;
+    for(int i=0;i<32;i++){
+        cpu->regs[i]=0;
+    }
+    cpu->mem=mem;
+    
+    return cpu;
+}
+
+/*! \param pData Array of 4 bytes
+    \retval The big-endian equivlent
+*/
+uint32_t to_big(const uint8_t *pData)
+{
+    return
+        (((uint32_t)pData[0])<<24)
+        |
+        (((uint32_t)pData[1])<<16)
+        |
+        (((uint32_t)pData[2])<<8)
+        |
+        (((uint32_t)pData[3])<<0);
+}
+
+mips_error mips_cpu_step(
+	mips_cpu_h state	//! Valid (non-empty) handle to a CPU
+)
+{
+    uint8_t buffer[4];
+    
+    mips_error err=mips_mem_read(
+        state->mem,		//!< Handle to target memory
+        state->pc,	//!< Byte address to start transaction at
+        4,	//!< Number of bytes to transfer
+        buffer	//!< Receives the target bytes
+    );
+    
+    if(err!=0){
+        return err;
+    }
+    
+    uint32_t instruction= to_big(buffer);
+    
+    uint32_t opcode =  (instruction>>26) & 0x3F;
+    uint32_t src1 = (instruction>> 21 ) & 0x1F;
+    uint32_t src2 = (instruction>> 16 ) & 0x1F;   
+    uint32_t dst = (instruction>> 11 ) & 0x1F;    
+    uint32_t shift = (instruction>> 6 ) & 0x1F ;
+    uint32_t function = (instruction>> 0 ) & 0x3F ;
+
+    if(opcode == 0){
+        // This is R-type
+        if(state->logLevel >= 2){
+            fprintf(state->logDst, "R-Type : dst=%u, src1=%u, src2=%u, shift=%u, function=%u.\n  instr=%08x\n",
+                dst, src1, src2, shift, function, instruction
+            );
+        }
+        
+        if(function ==  0x21){
+            if(state->logLevel >= 1){
+                fprintf(state->logDst, "addu %u, %u, %u.\n", dst, src1, src2);
+            }
+            uint32_t va=state->regs[src1];
+            uint32_t vb=state->regs[src2];
+            
+            uint32_t res=va+vb;
+            
+            state->regs[dst] = res; // NOTE: what about zero reg?
+            
+            // NOTE : What about updating the program counter
+            return mips_Success;
+        }else{
+            return mips_ErrorNotImplemented;
+        }
+    }else{
+        return mips_ErrorNotImplemented;
+    }
 }
--- src/rbw14/test_mips.cpp
+++ src/rbw14/test_mips.cpp
@@ -1,61 +1,131 @@
-#include "mips_test.h"
-#include "mips_hdr.h"
-
-
-using namespace std;
-
-
+#include "mips.h"
 
 int main()
 {
-	mips_mem_h mem=mips_mem_create_ram(
-		1<<20,
-		4
-		);
-	
-	mips_cpu_h cpu=mips_cpu_create(mem);
-
-
-	
-	
-	
-	mips_test_begin_suite();
+    mips_mem_h mem=mips_mem_create_ram(4096, 4);
     
+    mips_cpu_h cpu=mips_cpu_create(mem);
+    
+    mips_error e=mips_cpu_set_debug_level(cpu, 0, stderr);
+    if(e!=mips_Success){
+        fprintf(stderr, "mips_cpu_set_debug_level : failed.\n");
+        exit(1);
+    }
+    
+    
+    mips_test_begin_suite();
 
- 
- 
-	int testId=mips_test_begin_test("and");
-	int passed=0;
 
-	
-	mips_error err = mips_cpu_set_register(cpu, 8, 0x0000FFFFul);
-	if(err==0)
-		err = mips_cpu_set_register(cpu, 9, 0x00FFFF00ul);
-
-	// and $10, $8, $9
-
-	// TODO : work out the bit-wise encoding for the instruction.
-
-	// TODO : Write it into mempory at a known address
-
-	// TODO : Make sure the program-counter is at that address
-
-	if(err==0)
-		err=mips_cpu_step(cpu);
-
-	uint32_t got;
-	if(err==0)
-		err = (mips_error)(err | mips_cpu_get_register(cpu, 10, &got));
-
-	passed = (err == mips_Success) && (got==0x0000FF00);
-
-	mips_test_end_test(testId, passed, NULL);
-	//fclose(cpu->debug_out);
-	
-	mips_test_end_suite();
-	
-	mips_cpu_free(cpu);
-	mips_mem_free(mem);
-	
-	return 0;
+    int testId = mips_test_begin_test("addu");    
+    
+    // 1 - Setup an instruction in ram
+    // addu r3, r4, r5
+    uint32_t instr =
+        (0ul << 26) // opcode = 0
+        |
+        (4ul << 21) // srca = r4
+        |
+        (5ul << 16) // srcb = r5
+        |
+        (3ul << 11) // dst = r3
+        |
+        (0ul << 6) // shift = 0
+        |
+        (0x21 << 0);
+    
+    uint8_t buffer[4];
+    buffer[0]=(instr>>24)&0xFF;
+    buffer[1]=(instr>>16)&0xFF;
+    buffer[2]=(instr>>8)&0xFF;
+    buffer[3]=(instr>>0)&0xFF;
+    
+    e=mips_mem_write(
+        mem,	        //!< Handle to target memory
+        0,	            //!< Byte address to start transaction at
+        4,	            //!< Number of bytes to transfer
+        buffer	        //!< Receives the target bytes
+    );
+    if(e!=mips_Success){
+        fprintf(stderr, "mips_mem_write : failed.\n");
+        exit(1);
+    }
+    
+    // 2 - put register values in cpu
+    e=mips_cpu_set_register(cpu, 4, 40);
+    e=mips_cpu_set_register(cpu, 5, 50);
+    
+    // 3 - step CPU
+    e=mips_cpu_step(cpu);
+    if(e!=mips_Success){
+        fprintf(stderr, "mips_cpu_step : failed.\n");
+        exit(1);
+    }
+    
+    // 4 -Check the result
+    uint32_t got;
+    e=mips_cpu_get_register(cpu, 3, &got);
+    
+   
+    int passed = got == 40+50;
+    
+    mips_test_end_test(testId, passed, "50 + 50 != 90");
+    
+    
+    testId = mips_test_begin_test("addu");    
+    
+    // 1 - Setup an instruction in ram
+    // addu r3, r4, r5
+    instr =
+        (0ul << 26) // opcode = 0
+        |
+        (4ul << 21) // srca = r4
+        |
+        (5ul << 16) // srcb = r5
+        |
+        (0ul << 11) // dst = r0
+        |
+        (0ul << 6) // shift = 0
+        |
+        (0x21 << 0);
+    
+    buffer[0]=(instr>>24)&0xFF;
+    buffer[1]=(instr>>16)&0xFF;
+    buffer[2]=(instr>>8)&0xFF;
+    buffer[3]=(instr>>0)&0xFF;
+    
+    e=mips_mem_write(
+        mem,	        //!< Handle to target memory
+        0,	            //!< Byte address to start transaction at
+        4,	            //!< Number of bytes to transfer
+        buffer	        //!< Receives the target bytes
+    );
+    if(e!=mips_Success){
+        fprintf(stderr, "mips_mem_write : failed.\n");
+        exit(1);
+    }
+    
+    // 2 - put register values in cpu
+    e=mips_cpu_set_register(cpu, 4, 40);
+    e=mips_cpu_set_register(cpu, 5, 50);
+    
+    // 3 - step CPU
+    e=mips_cpu_step(cpu);
+    if(e!=mips_Success){
+        fprintf(stderr, "mips_cpu_step : failed.\n");
+        exit(1);
+    }
+    fprintf(stderr, "RAVI");
+    // 4 -Check the result
+    e=mips_cpu_get_register(cpu, 0, &got);
+    
+   
+    passed = got == 0;
+    
+    mips_test_end_test(testId, passed, "r0 <> 0");
+    
+    
+    
+    mips_test_end_suite();
+    
+    return 0;
 }
